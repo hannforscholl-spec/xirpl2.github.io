be280a6f47f8ad7756cb948532627f04ef879392
hannganteng <xirepeltwo@gmail.com>
2026-09-23 08:08:24 +0700

    i do somethink

--- src/components/site/Galeri.tsx
+++ src/components/site/Galeri.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 
 const items = [
   {
-    src: "fotbar.jpg",
+    src: "/photos/fotbar.jpg",
     caption: "Foto Bersama Kelas",
     meta: "Dokumentasi resmi",
     className: "sm:col-span-2 sm:row-span-2",
